ba1a4eff7bde12a3fde78a70163366e0d181e542
andretodee <andretodee@gmail.com>
2020-04-11 18:29:04 +0200

    test-5

--- main.js
+++ main.js
@@ -62,8 +62,6 @@ function slider(){
             $('.tab-name').removeClass('active');
             selectedTab.addClass('active');
         });
-
-
   };
 
   function mobileMenu(){
@@ -73,8 +71,6 @@ function slider(){
           $('.hamburger-menu').toggle();
       });
   };
-
-
 
   function validate() {
 
@@ -97,60 +93,6 @@ function slider(){
         message = '';
     }
 
-    // var tab1 = {
-    // 	"item": {
-    // 		"title": "Vestibulum at odio sit amet",
-    // 		"content": [
-    // 			"Vestibulum at odio sit amet diam consectetur congue.",
-    // 			"Donec imperdiet tincidunt nisi non dignissim.",
-    // 			"Maecenas diam metus, fermentum a velit ut, auctor consequat ligula.",
-    // 			"In ultrices lobortis venenatis.",
-    // 			"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque dignissim sit amet lectus ac tincidunt.",
-    // 			"Quisque bibendum mi at tempus tempus.",
-    // 			"Suspendisse pretium, quam eu faucibus cursus, nunc leo pharetra justo, ut rutrum lorem ipsum quis velit.",
-    // 			"Aenean imperdiet molestie dignissim.",
-    // 			"Curabitur faucibus nulla metus, vel ornare libero accumsan eget."
-    // 		]
-    // 	}
-    // }
-    //
-    // var tab2 = {
-    //     "item": {
-    //         "title": "Sed vehicula neque",
-    //         "content": [
-    //             "Donec ultricies felis non sem euismod, vel hendrerit metus porttitor.",
-    //             "Donec sed ex ut tellus ultricies vestibulum vitae sit amet erat.",
-    //             "Etiam tempor, libero eget bibendum auctor, risus nulla finibus dolor, in ullamcorper ipsum sapien dignissim felis.",
-    //             "Nunc mattis sagittis mi, at hendrerit dolor semper non.",
-    //             "Sed scelerisque sollicitudin felis euismod laoreet.",
-    //             "Nunc elementum purus orci, nec ornare mi vehicula at.",
-    //             "Praesent porta nisi in magna aliquam, sed consequat turpis ornare.",
-    //             "Sed laoreet porttitor purus vitae tincidunt."
-    //         ]
-    //     }
-    // }
-    //
-    // var tab3 = {
-    //     "item": {
-    //         "title": "Nulla id libero pretium",
-    //         "content": [
-    //             "Quisque massa leo, congue quis turpis et, ullamcorper consequat felis.",
-    //             "Nullam non vulputate elit.",
-    //             "Fusce id blandit tortor.",
-    //             "Nullam sagittis urna quis leo volutpat, nec porta nibh mollis.",
-    //             "Sed placerat, ligula sed condimentum fringilla, neque dui blandit dolor, eu semper quam nunc non magna.",
-    //             "Cras a hendrerit nisi.",
-    //             "Phasellus aliquam rutrum lectus, feugiat eleifend ex faucibus quis.",
-    //             "Aliquam sed quam tellus.",
-    //             "Duis porta vel nisi at tempus.",
-    //             "Suspendisse eu vestibulum odio, id ultricies mauris.",
-    //             "Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
-    //             "Aenean sit amet auctor tellus.",
-    //             "Vivamus sodales nisi fermentum euismod posuere."
-    //         ]
-    //     }
-    // }
-
     function getData(path){
         $.getJSON("./ajax-json/" + path + ".json", function(json) {
             console.log('test');
@@ -160,15 +102,8 @@ function slider(){
             var finalText = textArray.toString();
 
             $('.tab-text > p').html(finalText);
-
         });
-
-
-
-
-
     };
-
 
 function init(){
     slider();
